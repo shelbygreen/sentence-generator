da9c4a9a16937eeabfa51d71c7e6ef0051db101e
shelby <greenshelby@Gmail.com>
2022-11-07 21:52:19 -0500

    first commit! :)

---
+++ app.js
@@ -0,0 +1,23 @@
+$(function() {
+    changer();
+});
+
+function changer() {
+    var words = [
+        "AFFORDABLE HOUSING",
+        "BUSES THAT SHOW UP ON TIME",
+        "MORE PARKS",
+        "LOWER UTILITY BILLS",
+        "A PARK IN PROVIDENCE",
+        "THE RED LIGHT CAMERA PROGRAM TO COME BACK",
+        "LIVABLE WAGES",
+        "THE END OF POLICE BRUTALITY",
+        "A BETTER TALLAHASSEE",
+        "REALISTIC SOLUTIONS TO CURB GUN VIOLENCE",
+        "MORE BIKE LANES",
+        "CLEAN ENERGY"];
+    var idx = Math.floor(words.length * Math.random());
+    $('#change').text(words[idx]);
+    var time = 1500;
+    setTimeout(changer,time);
+}
